94ac1a428c566c061f2b74beba2ab4b6adda739c
MartinBINARD <martin.binard.dev@gmail.com>
2025-05-09 18:11:19 -1000

    Revert to java version 17 + Add H2 database

--- src/main/java/com/puzzle/solutionapi/entity/Solution.java
+++ src/main/java/com/puzzle/solutionapi/entity/Solution.java
@@ -1,6 +1,10 @@
 package com.puzzle.solutionapi.entity;
 
-import jakarta.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Solution {
